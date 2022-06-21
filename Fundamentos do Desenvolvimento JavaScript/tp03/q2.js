f11dca074c6d73281b48f9f72f3958b6387af396
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-06-20 23:43:23 -0300

    Finished question 02

--- Fundamentos do Desenvolvimento JavaScript/tp03/q2.js
+++ Fundamentos do Desenvolvimento JavaScript/tp03/q2.js
@@ -1,14 +1,42 @@
-let questionContainer = document.getElementById('q2');
+let questionTwoContainer = document.getElementById('q2');
 
 function createHtmlElement(tag, parent, properties) {
-   
+
     let htmlElement = document.createElement(tag);
     if (properties) {
         Object.entries(properties).forEach(([key, value]) => {
             htmlElement.setAttribute(key, value)
         });
     }
-    
+
     parent.appendChild(htmlElement);
     return htmlElement
+};
+
+let label = createHtmlElement('label', questionTwoContainer, {});
+let factorialInput = createHtmlElement('input', questionTwoContainer, { 'type': 'number', 'placeholder': 'maximo', 'required': 'true' });
+let factorialButton = createHtmlElement('input', questionTwoContainer, { 'type': 'button', 'value': 'Calcular', 'onclick': 'calculate()' });
+
+function createAnswer(number, factorial) {
+
+    let answerDiv = createHtmlElement('div', questionTwoContainer, {})
+    answerDiv.innerText = `O fatorial de ${number} é ${factorial}.`
+
+}
+
+function calculate() {
+    var number = Number(factorialInput.value);
+    let factorial = 1;
+    if (number == 0 || number == 1) {
+        createAnswer(number, factorial)
+        return factorial;
+        
+    } else {
+        for (var i = number; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        createAnswer(number, factorial)
+        return factorial;
+    }
+
 };
