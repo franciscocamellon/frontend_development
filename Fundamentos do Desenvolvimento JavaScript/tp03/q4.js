59ea0d52c2ff02adb11b4e2c004cd6cdc790ba3b
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-06-21 21:16:52 -0300

    end of the day commit

--- Fundamentos do Desenvolvimento JavaScript/tp03/q4.js
+++ Fundamentos do Desenvolvimento JavaScript/tp03/q4.js
@@ -12,3 +12,10 @@ function createHtmlElement(tag, parent, properties) {
     parent.appendChild(htmlElement);
     return htmlElement
 };
+
+let quantityLabel = createHtmlElement('label', questionFourContainer, {});
+quantityLabel.innerText = "Quantidade de números";
+let numberQuantity = createHtmlElement('input', questionFourContainer, { 'type': 'number', 'placeholder': 'quantidade', 'required': 'true' });
+let minValue = createHtmlElement('input', questionFourContainer, { 'type': 'number', 'placeholder': 'mínimo', 'required': 'true' });
+let maxValue = createHtmlElement('input', questionFourContainer, { 'type': 'number', 'placeholder': 'máximo', 'required': 'true'});
+let resultButton = createHtmlElement('input', questionFourContainer, { 'type': 'button', 'value': 'Calcular', 'onclick': 'calculateFactorial()' });
